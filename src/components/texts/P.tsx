5c752d3418e9a3425c07acc3b3037f408acdb32a
Sudhagar <deeplyncmailbox@gmail.com>
2021-06-19 15:07:31 +0530

    react native app sample code

---
+++ src/components/texts/P.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, TextProps } from 'react-native';
+import { FONT_SIZE_P } from '../../constants/ThingerStyles';
+import { COLOR_TEXT } from '../../constants/ThingerColors';
+
+export default class PText extends React.Component<TextProps> {
+	render() {
+		const { style, children, ...props } = this.props;
+		return (
+			<Text
+				style={[{ color: COLOR_TEXT, fontSize: FONT_SIZE_P }, style]}
+				{...props}
+			>
+				{children}
+			</Text>
+		);
+	}
+}
